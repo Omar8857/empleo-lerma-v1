b2300299293868987961854ff6e3f137c2ccc41e
Arturo PM <al221810751@gmail.com>
2020-08-27 19:34:14 -0600

    Empleo Lerma

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -13,49 +13,49 @@ return [
         'status_bar'=> 'black',
         'icons' => [
             '72x72' => [
-                'path' => '/empleo-lerma/public/images/icons/icon-72x72.png',
+                'path' => '/images/icons/icon-72x72.png',
                 'purpose' => 'any'
             ],
             '96x96' => [
-                'path' => '/empleo-lerma/public/images/icons/icon-96x96.png',
+                'path' => '/images/icons/icon-96x96.png',
                 'purpose' => 'any'
             ],
             '128x128' => [
-                'path' => '/empleo-lerma/public/images/icons/icon-128x128.png',
+                'path' => '/images/icons/icon-128x128.png',
                 'purpose' => 'any'
             ],
             '144x144' => [
-                'path' => '/empleo-lerma/public/images/icons/icon-144x144.png',
+                'path' => '/images/icons/icon-144x144.png',
                 'purpose' => 'any'
             ],
             '152x152' => [
-                'path' => '/empleo-lerma/public/images/icons/icon-152x152.png',
+                'path' => '/images/icons/icon-152x152.png',
                 'purpose' => 'any'
             ],
             '192x192' => [
-                'path' => '/empleo-lerma/public/images/icons/icon-192x192.png',
+                'path' => '/images/icons/icon-192x192.png',
                 'purpose' => 'any'
             ],
             '384x384' => [
-                'path' => '/empleo-lerma/public/images/icons/icon-384x384.png',
+                'path' => '/images/icons/icon-384x384.png',
                 'purpose' => 'any'
             ],
             '512x512' => [
-                'path' => '/empleo-lerma/public/images/icons/icon-512x512.png',
+                'path' => '/images/icons/icon-512x512.png',
                 'purpose' => 'any'
             ],
         ],
         'splash' => [
-            '640x1136' => '/empleo-lerma/public/images/icons/splash-640x1136.jpg',
-            '750x1334' => '/empleo-lerma/public/images/icons/splash-750x1334.jpg',
-            '828x1792' => '/empleo-lerma/public/images/icons/splash-828x1792.jpg',
-            '1125x2436' => '/empleo-lerma/public/images/icons/splash-1125x2436.jpg',
-            '1242x2208' => '/empleo-lerma/public/images/icons/splash-1242x2208.jpg',
-            '1242x2688' => '/empleo-lerma/public/images/icons/splash-1242x2688.jpg',
-            '1536x2048' => '/empleo-lerma/public/images/icons/splash-1536x2048.jpg',
-            '1668x2224' => '/empleo-lerma/public/images/icons/splash-1668x2224.jpg',
-            '1668x2388' => '/empleo-lerma/public/images/icons/splash-1668x2388.jpg',
-            '2048x2732' => '/empleo-lerma/public/images/icons/splash-2048x2732.jpg',
+            '640x1136' => '/images/icons/splash-640x1136.jpg',
+            '750x1334' => '/images/icons/splash-750x1334.jpg',
+            '828x1792' => '/images/icons/splash-828x1792.jpg',
+            '1125x2436' => '/images/icons/splash-1125x2436.jpg',
+            '1242x2208' => '/images/icons/splash-1242x2208.jpg',
+            '1242x2688' => '/images/icons/splash-1242x2688.jpg',
+            '1536x2048' => '/images/icons/splash-1536x2048.jpg',
+            '1668x2224' => '/images/icons/splash-1668x2224.jpg',
+            '1668x2388' => '/images/icons/splash-1668x2388.jpg',
+            '2048x2732' => '/images/icons/splash-2048x2732.jpg',
         ],
         'shortcuts' => [
             [
@@ -63,7 +63,7 @@ return [
                 'description' => 'Shortcut Link 1 Description',
                 'url' => '/shortcutlink1',
                 'icons' => [
-                    "src" => "/empleo-lerma/public/images/icons/icon-72x72.png",
+                    "src" => "/images/icons/icon-72x72.png",
                     "purpose" => "any"
                 ]
             ],
